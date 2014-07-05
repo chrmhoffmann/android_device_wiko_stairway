674fc6b2cf1c05b74a39afc4a9447b23b1d90723
chrmhoffmann <chrmhoffmann@gmail.com>
2014-07-05 18:26:24 +0200

    stairway: extract-files.sh use adb and fix sound lib.

--- extract-files.sh
+++ extract-files.sh
@@ -12,16 +12,11 @@ DIR=`dirname $FILE`
 mkdir -p $BASE/$DIR
     fi
 
-cp /work/chris/android/wiko/backup/system/$FILE $BASE/$FILE
-#adb pull /system/$FILE $BASE/$FILE
-
+adb pull /system/$FILE $BASE/$FILE
 done
 
 # some extra stuff
-cp /work/chris/android/wiko/backup/system/lib/hw/audio_policy.default.so $BASE/lib/hw/audio_policy.mt6589.so
-cp /work/chris/android/wiko/backup/system/lib/libaudio.primary.default.so $BASE/lib/hw/audio.primary.mt6589.so
-
-#adb pull /system/lib/hw/audio_policy.default.so $BASE/lib/hw/audio_policy.mt6589.so
-#adb pull /system/lib/hw/libaudio.r_submix.default.so $BASE/lib/hw/audio.r_submix.mt6589.so
+adb pull /system/lib/hw/audio_policy.default.so $BASE/lib/hw/audio_policy.mt6589.so
+adb pull /system/lib/libaudio.primary.default.so $BASE/lib/hw/audio.primary.mt6589.so
 
 ./setup-makefiles.sh
