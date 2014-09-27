10e0fffcbdec33b169d9f2fd5a5627823a8da3ec
chrmhoffmann <chrmhoffmann@gmail.com>
2014-09-27 14:50:08 +0200

    stairway: no audio policy blob. guard with device name if other mtk devices are in the tree.

--- extract-files.sh
+++ extract-files.sh
@@ -16,7 +16,6 @@ adb pull /system/$FILE $BASE/$FILE
 done
 
 # some extra stuff
-adb pull /system/lib/hw/audio_policy.default.so $BASE/lib/hw/audio_policy.mt6589.so
 adb pull /system/lib/libaudio.primary.default.so $BASE/lib/hw/audio.primary.mt6589.so
 
 ./setup-makefiles.sh
